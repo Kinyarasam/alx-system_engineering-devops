541b29e87bed7b4cf93b9484c2bf57923ea22b8b
kinyarasam <skinyara.30@gmail.com>
2022-10-17 23:05:46 +0300

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,6 +3,8 @@
 
 Returns:
     Employee information about his/her TODO list progress.
+Notes:
+    Implemented Using Recursion
 """
 
 import requests
@@ -10,7 +12,7 @@ import sys
 import re
 
 
-# REST API url
+"""REST API url"""
 REST_API = 'https://jsonplaceholder.typicode.com'
 ID = sys.argv[1]
 
