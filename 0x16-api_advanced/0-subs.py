533c9138e22c6c4f2329bbf64e978de164d774bc
kinyarasam <skinyara.30@gmail.com>
2022-11-03 16:34:56 +0300

    0. How many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# function that queries the Reddit API and returns the number of subscribers
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Get the number of Subscribers
+
+    Returns:
+        (int) number of subscribers for a given subreddit
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
+    REDDIT_API = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+
+    req = requests.get(REDDIT_API, headers={
+        'User-Agent': '0x16-api_advanced:project: v1.0.0 (by /u/kinyarasam)'
+        }).json()
+
+    subs = req.get("data", {}).get("subscribers", 0)
+    return subs
