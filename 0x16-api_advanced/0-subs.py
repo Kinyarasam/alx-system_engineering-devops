f3f95bb7de604b495d8691dc1c00c191d52f4798
kinyarasam <skinyara.30@gmail.com>
2022-11-03 16:38:15 +0300

    Documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,11 +13,14 @@ def number_of_subscribers(subreddit):
     if subreddit is None or type(subreddit) is not str:
         return 0
 
+    # Global variable declaration of the Reddit api
     REDDIT_API = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
 
+    # Request
     req = requests.get(REDDIT_API, headers={
         'User-Agent': '0x16-api_advanced:project: v1.0.0 (by /u/kinyarasam)'
         }).json()
 
+    # Get subs.
     subs = req.get("data", {}).get("subscribers", 0)
     return subs
