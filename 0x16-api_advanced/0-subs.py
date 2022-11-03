ce81627c44302d2cc6fa75f2d1f82f266c92ea06
kinyarasam <skinyara.30@gmail.com>
2022-11-03 20:23:39 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,10 @@ def number_of_subscribers(subreddit):
         return 0
 
     # Request
-    req = requests.get('http://www.reddit.com/r/{}/about.json'
-            .format(subreddit), headers={
-                'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/kinyarasam)'}).json()
+    req = requests.get('http://www.reddit.com/r/{}/about.json\
+'.format(subreddit), headers={
+                            'User-Agent': '0x16-api_advanced:project: v1.0.0\
+(by /u/kinyarasam)'}).json()
 
     # Get subs.
     subs = req.get("data", {}).get("subscribers", 0)
